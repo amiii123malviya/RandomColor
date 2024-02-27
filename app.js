0e73bf270e5895be607b428eac6da9eefcbfe7b4
amiii123malviya <147029096+amiii123malviya@users.noreply.github.com>
2024-02-27 20:59:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const getColor = () =>
+{
+    const randomNumber=Math.floor(Math.random() * 1677215);
+    const randomCode = "#" + randomNumber.toString(16);
+    console.log(randomNumber,randomCode);
+    document.body.style.backgroundColor = randomCode
+    document.getElementById("color-code").innerText = randomCode
+    navigator.clipboard.writeText(randomCode)
+}
+//function call
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+//init call
+getColor();
